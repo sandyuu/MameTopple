b574c183e8989198b8d79be1b8227d0053de096f
Cheng Ching Jen <win50117@gmail.com>
2020-10-10 03:15:48 +0800

    Feat:新增JWT的API實做，能夠取得Token。
    登入時呼叫API，取得Token，後續才可夾帶Token請求任何需要驗證的API
    TODO:登入資料與資料庫比對

---
+++ MameToppleApi/Controllers/TokenController.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MameToppleApi.Helpers;
+using MameToppleApi.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MameToppleApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly JwtHelpers _jwt;
+
+        public TokenController(JwtHelpers jwt)
+        {
+            _jwt = jwt;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult<string> SignIn(LoginViewModel login)
+        {
+            if (ValidateUser(login))
+            {
+                return _jwt.GenerateToken(login.Username);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        private bool ValidateUser(LoginViewModel login)
+        {
+            return true; // TODO 和資料庫比對使用者登入資料
+        }
+
+        [Authorize] //通過驗證才能存取
+        [HttpGet]
+        public IActionResult GetClaims()
+        {
+            return Ok(User.Claims.Select(p => new { p.Type, p.Value }));
+        }
+
+        [Authorize] //通過驗證才能存取
+        [HttpGet]
+        public IActionResult GetUserName()
+        {
+            return Ok(User.Identity.Name);
+        }
+
+        [HttpGet]
+        public IActionResult GetUniqueId() //取得Token的GUID
+        {
+            var jti = User.Claims.FirstOrDefault(p => p.Type == "jti");
+            return Ok(jti.Value);
+        }
+    }
+}
---
+++ MameToppleApi/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,8 @@
+namespace MameToppleApi.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
