a68eda7494bd95a809411d5460ff217e29395809
Cheng Ching Jen <win50117@gmail.com>
2020-10-20 22:48:39 +0800

    feat:完成兩個UserService測試

--- MameToppleApi.Tests/Services/UserServiceTest.cs
+++ MameToppleApi.Tests/Services/UserServiceTest.cs
@@ -1,13 +1,26 @@
+using System.Security.Cryptography.X509Certificates;
+using System;
+using System.Linq.Expressions;
 using MameToppleApi.Interfaces;
 using MameToppleApi.Models;
+using MameToppleApi.Models.ViewModels;
+using MameToppleApi.Service;
 using Moq;
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace MameToppleApi.Tests.Services
 {
     [TestFixture]
     public class UserServiceTest
     {
+        private List<User> fakeUserList = new List<User>()
+        {
+            new User{Account="t1",Password="t1",Avatar="t1",NickName="t1",Win=0,Lose=0},
+            new User{Account="t2",Password="t2",Avatar="t2",NickName="t2",Win=0,Lose=0},
+            new User{Account="t3",Password="t3",Avatar="t3",NickName="t3",Win=0,Lose=0}
+        };
+
         private Mock<IRepository<User>> _genericRepositoryMock;
 
         [SetUp]
@@ -17,18 +30,60 @@ namespace MameToppleApi.Tests.Services
         }
 
         [Test]
-        public void User_GetUser_Return_UserVM()
+        public void When_GiveAccount_Then_GetUserVM()
         {
             //Arrange 設定初始資料、期望結果
-
-
-
+            Expression<Func<User, bool>> fakeEx = x => x.Account == "Test@gmail.com";
+            User fakeUser = new User
+            {
+                Account = "admin@gmial.com",
+                Password = "123456",
+                Avatar = "Mame",
+                NickName = "Admin01",
+                Win = 0,
+                Lose = 0
+            };
+            _genericRepositoryMock.Setup(x => x.GetOne(It.IsAny<Expression<Func<User, bool>>>())).Returns(fakeUser);
+            UserService service = new UserService(_genericRepositoryMock.Object);
 
             //Act 呼叫哪個方法進行測試
-
+            var actual = service.GetById("admin@gmail.com");
             //Assert 比較預期結果和實際運行結果，應該要相等
-
+            Assert.IsInstanceOf(typeof(UserViewModel), actual);
         }
 
+        [Test]
+        public void When_LoginModel_UserNotExist_Then_ReturnFalse()
+        {
+            //Arrange 安排        
+            LoginViewModel fakeLoginVM = new LoginViewModel
+            {
+                Account = "t4",
+                Password = "t4"
+            };
+            _genericRepositoryMock.Setup(X => X.GetAllAsync()).ReturnsAsync(fakeUserList);
+            UserService userService = new UserService(_genericRepositoryMock.Object);
+            //Act 執行
+            var actual = userService.LoginVerify(fakeLoginVM).Result;
+            //Assert 斷言
+            Assert.AreEqual(actual, false);
+        }
+
+        [Test]
+        public void When_LoginModel_UserIsExist_Then_ReturnTrue()
+        {
+            //Arrange 安排
+            LoginViewModel fakeLoginVM = new LoginViewModel
+            {
+                Account = "t1",
+                Password = "t1"
+            };
+            _genericRepositoryMock.Setup(X => X.GetAllAsync()).ReturnsAsync(fakeUserList);
+            UserService userService = new UserService(_genericRepositoryMock.Object);
+            //Act 執行
+            var actual = userService.LoginVerify(fakeLoginVM).Result;
+            //Assert 斷言
+            Assert.AreEqual(actual, true);
+        }
     }
 }
